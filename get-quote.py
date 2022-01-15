bbff8a017e0eb83907d800dd627eef335a5bca3e
JERErmitaño <jermitano.tech@gmail.com>
2022-01-15 13:01:30 +0800

    Update step 3

--- get-quote.py
+++ get-quote.py
@@ -6,7 +6,7 @@ def primary():
   last = len(quotes) - 3
   rnd = random.randint(0, last)
   rnd1 = random.randint(0,last)
-  print(quotes[rnd],quotes[rnd1],end='',)
+  print(quotes[rnd],quotes[rnd1],end='')
 
 if __name__== "__main__":
   primary()
